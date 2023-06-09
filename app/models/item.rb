7fa33a87b4639810182f299ae856d9466cc8352a
Myles Nottingham <MylesNottingham@gmail.com>
2023-06-08 18:58:47 -0600

    Refactor: app directory per Rubocop

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ class Item < ApplicationRecord
 
   def best_day
     invoices
-    .joins(:invoice_items)
-    .where('invoices.status = 2')
-    .select('invoices.*, sum(invoice_items.unit_price * invoice_items.quantity) as money')
-    .group(:id)
-    .order("money desc", "created_at desc")
-    .first&.created_at&.to_date
+      .joins(:invoice_items)
+      .where("invoices.status = 2")
+      .select("invoices.*, sum(invoice_items.unit_price * invoice_items.quantity) as money")
+      .group(:id)
+      .order("money desc", "created_at desc")
+      .first&.created_at&.to_date
   end
 end
