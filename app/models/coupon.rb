7acbace69d94572d766db0fccf22651b58021aa0
Myles Nottingham <MylesNottingham@gmail.com>
2023-06-10 16:54:52 -0600

    Feat: Add instance method to coupon model

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,14 +2,19 @@ class Coupon < ApplicationRecord
   validates_presence_of :name, :merchant_id
   validates :code, uniqueness: {case_sensitive: false}, presence: true
   validates :value, presence: true, numericality: {greater_than: 0}
+  before_save { code.upcase! }
 
   belongs_to :merchant
   has_many :invoices
 
-  enum activation_status: {active: true, inactive: false}
+  enum activation_status: {Active: true, Inactive: false}
 
   # instance methods
   def times_used
     invoices.where("invoices.status = 2").count
+  end
+
+  def invoices_in_progress?
+    invoices.where("invoices.status = 1").count > 0
   end
 end
