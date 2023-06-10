a1464c1305a906222fad760473500538228368fa
Myles Nottingham <MylesNottingham@gmail.com>
2023-06-10 13:15:56 -0600

    Test: Test for enhanced validations

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,11 +1,28 @@
 require "rails_helper"
 
 RSpec.describe Coupon, type: :model do
+  before :each do
+    @merchant = Merchant.create!(name: "Hair Care")
+    @coupon = Coupon.create!(
+      name: "Anniversary Sale",
+      code: "ANNIV10",
+      value: 10,
+      percent_not_dollar: true,
+      merchant: @merchant
+    )
+  end
+
   describe "validations" do
+    subject { @coupon }
+
     it { should validate_presence_of :name }
-    it { should validate_presence_of :code }
-    it { should validate_presence_of :value }
     it { should validate_presence_of :merchant_id }
+
+    it { should validate_presence_of :code }
+    it { should validate_uniqueness_of(:code).case_insensitive }
+
+    it { should validate_presence_of :value }
+    it { should validate_numericality_of(:value).is_greater_than(0) }
   end
 
   describe "relationships" do
