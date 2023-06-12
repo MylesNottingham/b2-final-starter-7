0f320bdc7f29b4d4574faa9c2cd1e42608ebe5ee
Myles Nottingham <MylesNottingham@gmail.com>
2023-06-12 17:40:45 -0600

    Feat: Add invoice model instance methods

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,7 +11,20 @@ class Invoice < ApplicationRecord
 
   enum status: [:cancelled, :in_progress, :completed]
 
+  # instance methods
   def total_revenue
     invoice_items.sum("unit_price * quantity")
+  end
+
+  def discount
+    if coupon.percent_not_dollar
+      total_revenue * (coupon.value / 100.to_f)
+    else
+      coupon.value
+    end
+  end
+
+  def total_revenue_with_discount
+    coupon ? [total_revenue - discount, 0].max : total_revenue
   end
 end
